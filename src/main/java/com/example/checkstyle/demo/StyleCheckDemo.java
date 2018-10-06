ba12f48d437a91a7d7f09bfc9ecbe60e9d148345
Jay He <hejian811@163.com>
2018-10-06 19:41:21 +0800

    init commit

---
+++ src/main/java/com/example/checkstyle/demo/StyleCheckDemo.java
@@ -0,0 +1,17 @@
+package com.example.checkstyle.demo;
+
+
+/**
+ *
+ */
+public class StyleCheckDemo {
+
+    public StyleCheckDemo() {
+    }
+
+    public void testTededdEededDDddd() {
+        System.out.print("here we go");
+        String testValue2 = "test";
+        System.out.print(testValue2.replace("test", "ttt"));
+    }
+}
